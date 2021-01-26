5bf0aacd0f98017bc4d86545c04ec38d99022d78
eneri-jr <eneri.jr@gmail.com>
2021-01-26 15:20:23 -0300

    Insercao de Constraints nas classes que tem valor unico

--- casa-do-codigo/src/main/java/br/com/zup/casadocodigo/autor/Autor.java
+++ casa-do-codigo/src/main/java/br/com/zup/casadocodigo/autor/Autor.java
@@ -2,6 +2,7 @@ package br.com.zup.casadocodigo.autor;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Autor {
 
 	@NotBlank
 	@Email
+	@Column(unique = true)
 	private String email;
 
 	@NotBlank
--- casa-do-codigo/src/main/java/br/com/zup/casadocodigo/pais/Pais.java
+++ casa-do-codigo/src/main/java/br/com/zup/casadocodigo/pais/Pais.java
@@ -1,5 +1,6 @@
 package br.com.zup.casadocodigo.pais;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class Pais {
 	private Long id;
 
 	@NotBlank
+	@Column(unique = true)
 	private String nome;
 
 	public Pais() {
